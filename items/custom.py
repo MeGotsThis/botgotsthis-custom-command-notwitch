2575b7b34a5af652ee37a7375e444d42563ec66b
MeGotsThis <megotsthis@gmail.com>
2017-09-27 11:54:56 -0700

    Adding files

---
+++ items/custom.py
@@ -0,0 +1,22 @@
+﻿from typing import Collection, Iterable
+
+from lib.data import CustomCommandField, CustomCommandProcess
+
+from .. import custom
+
+
+def fields() -> Iterable[CustomCommandField]:
+    return []
+
+
+def properties() -> Collection[str]:
+    if not hasattr(properties, 'properties'):
+        setattr(properties, 'properties', [
+            'notwitch',
+            'notwitchpartial',
+            ])
+    return getattr(properties, 'properties')
+
+
+def postProcess() -> Iterable[CustomCommandProcess]:
+    yield custom.propertyNoTwitchCommands

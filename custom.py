93a7e22ccd0e1b623c286deb141c2e663b899998
MeGotsThis <megotsthis@gmail.com>
2017-11-19 14:40:53 -0800

    Fixed the code to support BGT 8.0

--- custom.py
+++ custom.py
@@ -2,14 +2,14 @@ from lib.data import CustomProcessArgs
 
 
 async def propertyNoTwitchCommands(args: CustomProcessArgs) -> None:
-    if not await args.database.getCustomCommandProperty(
+    if not await args.data.getCustomCommandProperty(
             args.broadcaster, args.level, args.command, 'notwitch'):
         return
 
     if any(m for m in args.messages if m.startswith(('.', '/'))):
         messages = args.messages[:]
         args.messages.clear()
-        if await args.database.getCustomCommandProperty(
+        if await args.data.getCustomCommandProperty(
                 args.broadcaster, args.level, args.command, 'notwitchpartial'):
             args.messages.extend(m for m in messages
                                  if not m.startswith(('.', '/')))
